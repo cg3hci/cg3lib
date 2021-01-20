8125f384e9ca86c04b4d672ba8ea9f346858dfc2
alemuntoni <muntoni.alessandro@gmail.com>
2021-01-20 11:50:23 +0100

    viewer cmake working - this first draft should build everything - still need to test

--- cg3/viewer.cmake
+++ cg3/viewer.cmake
@@ -92,16 +92,78 @@ if (CG3_STATIC)
 	set(CG3_VIEWER_RESOURCES
 		${CMAKE_CURRENT_LIST_DIR}/viewer/internal/icons/icons.qrc)
 	
+	set(CG3_VIEWER_LINK_LIBS
+		cg3-core 
+		${QGLVIEWER_LIBRARY}
+		Qt5::Core
+		Qt5::OpenGL
+		Qt5::Widgets
+		Qt5::Xml)
+	
+	if (TARGET cg3-meshes)
+		list(APPEND CG3_VIEWER_HEADERS
+			${CMAKE_CURRENT_LIST_DIR}/viewer/drawable_objects/drawable_dcel.h
+			${CMAKE_CURRENT_LIST_DIR}/viewer/pickable_objects/pickable_dcel.h
+			${CMAKE_CURRENT_LIST_DIR}/viewer/managers/dcel_manager.h)
+
+		list(APPEND CG3_VIEWER_SOURCES
+			${CMAKE_CURRENT_LIST_DIR}/viewer/drawable_objects/drawable_dcel.cpp
+			${CMAKE_CURRENT_LIST_DIR}/viewer/managers/dcel_manager.cpp
+			${CMAKE_CURRENT_LIST_DIR}/viewer/pickable_objects/pickable_dcel.cpp)
+
+		list(APPEND CG3_VIEWER_FORMS
+			${CMAKE_CURRENT_LIST_DIR}/viewer/managers/dcel_manager.ui)
+		
+		if(TARGET Eigen3::Eigen)
+			list(APPEND CG3_VIEWER_HEADERS
+				${CMAKE_CURRENT_LIST_DIR}/viewer/drawable_objects/drawable_eigenmesh.h
+				${CMAKE_CURRENT_LIST_DIR}/viewer/pickable_objects/pickable_eigenmesh.h
+				${CMAKE_CURRENT_LIST_DIR}/viewer/managers/eigenmesh_manager.h)
+	
+			list(APPEND CG3_VIEWER_SOURCES
+				${CMAKE_CURRENT_LIST_DIR}/viewer/drawable_objects/drawable_eigenmesh.cpp
+				${CMAKE_CURRENT_LIST_DIR}/viewer/managers/eigenmesh_manager.cpp
+				${CMAKE_CURRENT_LIST_DIR}/viewer/pickable_objects/pickable_eigenmesh.cpp)
+	
+			list(APPEND CG3_VIEWER_FORMS
+				${CMAKE_CURRENT_LIST_DIR}/viewer/managers/eigenmesh_manager.ui)
+			
+			if (TARGET cg3-cgal AND TARGET cg3-libigl)
+				list(APPEND CG3_VIEWER_HEADERS
+					${CMAKE_CURRENT_LIST_DIR}/viewer/managers/booleans_manager.h)
+				
+				list(APPEND CG3_VIEWER_SOURCES
+					${CMAKE_CURRENT_LIST_DIR}/viewer/managers/booleans_manager.cpp)
+				
+				list(APPEND CG3_VIEWER_FORMS
+					${CMAKE_CURRENT_LIST_DIR}/viewer/managers/booleans_manager.ui)
+				
+				list(APPEND CG3_VIEWER_LINK_LIBS cg3-cgal cg3-libigl)
+			endif()
+		endif()
+		
+		list(APPEND CG3_VIEWER_LINK_LIBS cg3-meshes)
+	endif()
+	
+	if (TARGET cg3-cinolib)
+		add_definitions(-DCINOLIB_USES_OPENGL)
+		add_definitions(-DCINOLIB_USES_QT)
+		add_definitions(-DGL_GLEXT_PROTOTYPES)
+		
+		list(APPEND CG3_VIEWER_HEADERS
+			${CMAKE_CURRENT_LIST_DIR}/viewer/drawable_objects/drawable_tetmesh.h)
+		
+		list(APPEND CG3_VIEWER_SOURCES
+			${CMAKE_CURRENT_LIST_DIR}/viewer/drawable_objects/drawable_tetmesh.cpp)
+		
+		list(APPEND CG3_VIEWER_LINK_LIBS GL GLU cg3-cinolib)
+	endif()
+	
 	add_library(
 		cg3-viewer SHARED 
 		${CG3_VIEWER_HEADERS} ${CG3_VIEWER_SOURCES} ${CG3_VIEWER_FORMS} ${CG3_VIEWER_RESOURCES})
 	
 	target_link_libraries(
 		cg3-viewer PUBLIC 
-			cg3-core 
-			${QGLVIEWER_LIBRARY}
-			Qt5::Core
-			Qt5::OpenGL
-			Qt5::Widgets
-			Qt5::Xml)
+			${CG3_VIEWER_LINK_LIBS})
 endif()
